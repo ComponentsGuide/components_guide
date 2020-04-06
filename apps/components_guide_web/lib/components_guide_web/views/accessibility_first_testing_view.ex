8e12b10a8d03aed1345b201580e5e192ff8e3806
Patrick Smith <patrick@burntcaramel.com>
2020-04-06 19:36:17 +1000

    Improve cheatsheet

--- apps/components_guide_web/lib/components_guide_web/views/accessibility_first_testing_view.ex
+++ apps/components_guide_web/lib/components_guide_web/views/accessibility_first_testing_view.ex
@@ -2,6 +2,7 @@ defmodule ComponentsGuideWeb.AccessibilityFirstTestingView do
   use ComponentsGuideWeb, :view
   require EEx
   use ComponentsGuideWeb.Snippets
+  use Phoenix.HTML
 
   def header_background do
     Styling.linear_gradient("150grad", [
@@ -14,7 +15,13 @@ defmodule ComponentsGuideWeb.AccessibilityFirstTestingView do
   def topic_article(assigns, block) do
     ~E"""
     <article class="mb-8">
-      <h2 class="mb-2 text-4xl leading-normal text-teal-800"><%= @title %></h2>
+      <h2 class="mb-2 text-4xl leading-normal text-teal-800">
+        <%= if Keyword.has_key?(assigns, :link) do %>
+          <%= link @title, to: @link %>
+        <% else %>
+          <%= @title %>
+        <% end %>
+      </h2>
       <%= block[:do] %>
     </article>
     """
@@ -24,5 +31,14 @@ defmodule ComponentsGuideWeb.AccessibilityFirstTestingView do
     ~E"""
     <pre><code class='<%= "lang-#{type}" %>'><%= code %></code></pre>
     """
+  end
+
+  defmodule Theme do
+    defstruct text_color: "blue"
+
+    def h2(theme = %Theme{}, content, attrs) do
+      class = "mb-2 text-4xl leading-normal text-#{theme.text_color}-800"
+      content_tag(:h2, content, [ {:class, class} | attrs ])
+    end
   end
 end
