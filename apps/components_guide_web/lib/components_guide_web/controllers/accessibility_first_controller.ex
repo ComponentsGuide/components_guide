1d0d8d3a2d54fa49f3844a5b486922e9eac53524
Patrick Smith <patrick@burntcaramel.com>
2020-04-24 17:53:06 +1000

    Add landmarks

--- apps/components_guide_web/lib/components_guide_web/controllers/accessibility_first_controller.ex
+++ apps/components_guide_web/lib/components_guide_web/controllers/accessibility_first_controller.ex
@@ -1,11 +1,20 @@
 defmodule ComponentsGuideWeb.AccessibilityFirstController do
   use ComponentsGuideWeb, :controller
+  require Logger
 
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
-  def show(conn, _params) do
+  def show(conn, %{"id" => "landmarks"}) do
+    render(conn, "landmarks.html")
+  end
+
+  def show(conn, %{"id" => "roles-cheatsheet"}) do
     render(conn, "roles-cheatsheet.html")
+  end
+
+  def show(conn, _params) do
+    raise Phoenix.Router.NoRouteError, conn: conn, router: ComponentsGuideWeb.Router
   end
 end
