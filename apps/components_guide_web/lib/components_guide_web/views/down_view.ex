b40eb397b243ae9696f2103532751a0f53f1a093
Patrick Smith <patrick@burntcaramel.com>
2020-05-13 21:48:22 +1000

    Improve down with referenced links

--- apps/components_guide_web/lib/components_guide_web/views/down_view.ex
+++ apps/components_guide_web/lib/components_guide_web/views/down_view.ex
@@ -2,17 +2,13 @@ defmodule ComponentsGuideWeb.DownView do
   use ComponentsGuideWeb, :view
 
   defmodule ParserState do
-    defstruct html: [], italics: nil, bold: nil, link: nil
+    defstruct html: [], italics: nil, bold: nil, link: nil, link_references: %{}
 
-    @spec parse(binary) ::
-            {:safe,
-             binary
-             | maybe_improper_list(
-                 binary | maybe_improper_list(any, binary | []) | byte,
-                 binary | []
-               )}
-    def parse(input) when is_binary(input) do
-      next(%__MODULE__{}, input)
+    def parse(input, options) when is_binary(input) do
+      %__MODULE__{
+        link_references: Access.get(options, :links, %{})
+      }
+      |> next(input)
     end
 
     # Links
@@ -40,13 +36,14 @@ defmodule ComponentsGuideWeb.DownView do
       |> next(rest)
     end
 
-    defp next(
-           state = %__MODULE__{link: {:capturing_url, text_chars, url_chars}},
-           <<")"::utf8>> <> rest
-         ) do
-      text = text_chars |> Enum.reverse() |> List.to_string()
-      url = url_chars |> Enum.reverse() |> List.to_string()
+    defp next(state = %__MODULE__{link: {:ready_for_url, text_chars}}, <<"["::utf8>> <> rest) do
+      %__MODULE__{state | link: {:capturing_url_reference, text_chars, []}}
+      |> next(rest)
+    end
 
+    defp next(state = %__MODULE__{link: {:ready_for_url, text_chars}}, rest) do
+      text = text_chars |> Enum.reverse() |> List.to_string()
+      url = Map.get(state.link_references, text, "")
       anchor_html = Phoenix.HTML.Link.link(text, to: url) |> Phoenix.HTML.safe_to_string()
 
       %__MODULE__{state | html: [anchor_html | state.html], link: nil}
@@ -54,10 +51,46 @@ defmodule ComponentsGuideWeb.DownView do
     end
 
     defp next(
-           state = %__MODULE__{link: {:capturing_url, link_chars, url_chars}},
+           state = %__MODULE__{link: {:capturing_url, text_chars, url_chars}},
+           <<")"::utf8>> <> rest
+         ) do
+      text = text_chars |> Enum.reverse() |> List.to_string()
+      url = url_chars |> Enum.reverse() |> List.to_string()
+      anchor_html = Phoenix.HTML.Link.link(text, to: url) |> Phoenix.HTML.safe_to_string()
+
+      %__MODULE__{state | html: [anchor_html | state.html], link: nil}
+      |> next(rest)
+    end
+
+    defp next(
+           state = %__MODULE__{link: {:capturing_url, text_chars, url_chars}},
            <<char::utf8>> <> rest
          ) do
-      %__MODULE__{state | link: {:capturing_url, link_chars, [char | url_chars]}}
+      %__MODULE__{state | link: {:capturing_url, text_chars, [char | url_chars]}}
+      |> next(rest)
+    end
+
+    defp next(
+           state = %__MODULE__{link: {:capturing_url_reference, text_chars, url_reference_chars}},
+           <<"]"::utf8>> <> rest
+         ) do
+      text = text_chars |> Enum.reverse() |> List.to_string()
+      url_reference = url_reference_chars |> Enum.reverse() |> List.to_string()
+      url = Map.get(state.link_references, url_reference, "")
+      anchor_html = Phoenix.HTML.Link.link(text, to: url) |> Phoenix.HTML.safe_to_string()
+
+      %__MODULE__{state | html: [anchor_html | state.html], link: nil}
+      |> next(rest)
+    end
+
+    defp next(
+           state = %__MODULE__{link: {:capturing_url_reference, text_chars, url_reference_chars}},
+           <<char::utf8>> <> rest
+         ) do
+      %__MODULE__{
+        state
+        | link: {:capturing_url_reference, text_chars, [char | url_reference_chars]}
+      }
       |> next(rest)
     end
 
@@ -106,7 +139,7 @@ defmodule ComponentsGuideWeb.DownView do
     end
   end
 
-  def down(input) when is_binary(input) do
-    ParserState.parse(input)
+  def down(input, options \\ %{}) when is_binary(input) do
+    ParserState.parse(input, options)
   end
 end
--- apps/components_guide_web/test/collected_live_web/views/down_view_test.exs
+++ apps/components_guide_web/test/collected_live_web/views/down_view_test.exs
@@ -25,5 +25,42 @@ defmodule ComponentsGuideWeb.DownViewTest do
       assert Subject.down("text with [link](https://example.org/) somewhere") ==
                raw("text with <a href=\"https://example.org/\">link</a> somewhere")
     end
+
+    test "link with self as reference" do
+      assert Subject.down("text with [link somewhere] interesting",
+               links: %{
+                 "link somewhere" => "https://example.org/"
+               }
+             ) ==
+               raw("text with <a href=\"https://example.org/\">link somewhere</a> interesting")
+    end
+
+    test "link with text as reference" do
+      assert Subject.down("text with [link somewhere][example org] interesting",
+               links: %{
+                 "example org" => "https://example.org/"
+               }
+             ) ==
+               raw("text with <a href=\"https://example.org/\">link somewhere</a> interesting")
+    end
+
+    test "link with number as reference" do
+      assert Subject.down("text with [link somewhere][1] interesting",
+               links: %{
+                 "1" => "https://example.org/"
+               }
+             ) ==
+               raw("text with <a href=\"https://example.org/\">link somewhere</a> interesting")
+    end
+
+    test "multiple links" do
+      assert Subject.down("text with [link somewhere] interesting and [another one] to visit",
+               links: %{
+                 "link somewhere" => "https://example.org/somewhere",
+                 "another one" => "https://example.org/another"
+               }
+             ) ==
+               raw("text with <a href=\"https://example.org/somewhere\">link somewhere</a> interesting and <a href=\"https://example.org/another\">another one</a> to visit")
+    end
   end
 end
