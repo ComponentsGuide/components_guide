0f0f98b73c43c1c54428655f7d4bf3ffbbf8db02
Patrick Smith <patrick@burntcaramel.com>
2020-05-23 22:14:46 +1000

    Format JavaScript

--- apps/components_guide_web/assets/js/app.js
+++ apps/components_guide_web/assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import css from "../css/app.css";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -10,25 +10,28 @@ import css from "../css/app.css"
 // Import dependencies
 //
 import "phoenix_html";
-import {Socket} from "phoenix"
-import NProgress from "nprogress"
-import LiveSocket from "phoenix_live_view"
+import { Socket } from "phoenix";
+import NProgress from "nprogress";
+import LiveSocket from "phoenix_live_view";
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: { _csrf_token: csrfToken }
+});
 
 // Show progress bar on live navigation and form submits
-window.addEventListener("phx:page-loading-start", info => NProgress.start())
-window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+window.addEventListener("phx:page-loading-start", info => NProgress.start());
+window.addEventListener("phx:page-loading-stop", info => NProgress.done());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)
-window.liveSocket = liveSocket
-
+window.liveSocket = liveSocket;
 
 import Vue from "vue";
 import VueEditor from "./editor.vue";
