279a85192fec4fdf3da4e79926bc5460079ac183
Patrick Smith <pgwsmith@gmail.com>
2021-03-18 05:01:15 +0000

    Add static plug for collected images

--- apps/components_guide_web/lib/components_guide_web/endpoint.ex
+++ apps/components_guide_web/lib/components_guide_web/endpoint.ex
@@ -7,44 +7,55 @@ defmodule ComponentsGuideWeb.Endpoint do
     signing_salt: "BfF2quM8"
   ]
 
-  socket "/socket", ComponentsGuideWeb.UserSocket,
+  socket("/socket", ComponentsGuideWeb.UserSocket,
     websocket: true,
     longpoll: false
+  )
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket("/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]])
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
+  plug(Plug.Static,
     at: "/",
     from: :components_guide_web,
     gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt collected)
+  )
+
+  plug(Plug.Static,
+    at: "/collected/image",
+    from: {:components_guide_web, "priv/static/collected/image"},
+    gzip: false,
+    only: ~w(svg png jpg gif)
+  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
+    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+    plug(Phoenix.LiveReloader)
+    plug(Phoenix.CodeReloader)
   end
 
-  plug Phoenix.LiveDashboard.RequestLogger,
+  plug(Phoenix.LiveDashboard.RequestLogger,
     param_key: "request_logger",
     cookie_key: "request_logger"
+  )
 
-  plug Plug.RequestId
-  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+  plug(Plug.RequestId)
+  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
 
-  plug Plug.Parsers,
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
-  plug Plug.Session, @session_options
-  plug ComponentsGuideWeb.Router
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
+  plug(Plug.Session, @session_options)
+  plug(ComponentsGuideWeb.Router)
 end
