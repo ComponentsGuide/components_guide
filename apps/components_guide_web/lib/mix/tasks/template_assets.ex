8f438f211acf7705d9cec1bb8745dac616446f4e
Patrick Smith <pgwsmith@gmail.com>
2021-03-18 22:26:39 +0000

    Log paths

--- apps/components_guide_web/lib/mix/tasks/template_assets.ex
+++ apps/components_guide_web/lib/mix/tasks/template_assets.ex
@@ -7,7 +7,11 @@ defmodule Mix.Tasks.TemplateAssets do
   def run(_) do
     Mix.Task.run("app.start")
 
+    IO.puts("Running mix template_assets in #{project_dir()}")
+    IO.puts("Template path: #{templates_path()}")
+
     image_paths = Path.wildcard(Path.join(templates_path(), "/**/*.{png,jpg,jpeg,gif}"))
+
     for image_path <- image_paths do
       process_image(image_path)
     end
@@ -20,23 +24,27 @@ defmodule Mix.Tasks.TemplateAssets do
     Mix.shell().info("Processed #{count} file(s).")
   end
 
-  defp project_dir(), do: File.cwd!
-  defp templates_path(), do: Path.join(project_dir(), "/apps/components_guide_web/lib/components_guide_web/templates")
-  defp static_collected_dir(), do: Path.join(project_dir(), "/apps/components_guide_web/priv/static/collected")
+  defp project_dir(), do: File.cwd!()
+
+  defp templates_path(),
+    do: Path.join(project_dir(), "/apps/components_guide_web/lib/components_guide_web/templates")
+
+  defp static_collected_dir(),
+    do: Path.join(project_dir(), "/apps/components_guide_web/priv/static/collected")
 
   defp process_image(image_path) do
     data = File.read!(image_path)
     media_type = MIME.from_path(image_path)
     hash = :crypto.hash(:sha256, data)
     hash_base64 = hash |> Base.url_encode64()
-    
+
     destination_dir = Path.join(static_collected_dir(), media_type)
     File.mkdir_p!(destination_dir)
-    
+
     extension = MIME.extensions(media_type) |> List.first()
     destination_path = Path.join(destination_dir, "#{hash_base64}.#{extension}")
     File.copy!(image_path, destination_path)
-    
+
     Mix.shell().info("Copied #{image_path} to #{destination_path}")
   end
 end
