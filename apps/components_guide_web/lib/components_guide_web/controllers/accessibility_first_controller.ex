4c0cf63241d489cd95ce653d4038bed0f62273a2
Patrick Smith <pgwsmith@gmail.com>
2021-04-02 10:13:05 +1100

    Use better og images

--- apps/components_guide_web/lib/components_guide_web/controllers/accessibility_first_controller.ex
+++ apps/components_guide_web/lib/components_guide_web/controllers/accessibility_first_controller.ex
@@ -3,24 +3,43 @@ defmodule ComponentsGuideWeb.AccessibilityFirstController do
   require Logger
 
   def index(conn, _params) do
-    render(conn, "index.html", article: "intro")
+    conn
+    |> assign(:page_title, page_title(nil))
+    |> render("index.html", article: "intro")
   end
 
   def show(conn, %{"id" => "widgets-cheatsheet"}) do
-    render(conn, "widgets-cheatsheet.html")
+    conn
+    |> assign(:page_title, "Accessible Widgets Cheatsheet")
+    |> render("widgets-cheatsheet.html")
   end
 
   def show(conn, %{"id" => "properties-cheatsheet"}) do
-    render(conn, "properties-cheatsheet.html")
+    conn
+    |> assign(:page_title, "Accessible Properties Cheatsheet")
+    |> render("properties-cheatsheet.html")
   end
 
   @articles ["navigation", "landmarks", "roles", "accessible-name", "forms", "content"]
 
   def show(conn, %{"id" => article}) when article in @articles do
-    render(conn, "index.html", article: article)
+    conn
+    |> assign(:page_title, page_title(article))
+    |> render("index.html", article: article)
   end
 
   def show(conn, _params) do
     raise Phoenix.Router.NoRouteError, conn: conn, router: ComponentsGuideWeb.Router
+  end
+
+  defp page_title("navigation"), do: "Accessibility-First Navigation"
+  defp page_title("landmarks"), do: "Accessibility-First Landmarks"
+  defp page_title("roles"), do: "Accessibility-First Roles"
+  defp page_title("forms"), do: "Accessibility-First Forms"
+  defp page_title("content"), do: "Accessibility-First Content"
+  defp page_title("accessible-name"), do: "Learning Accessible Names"
+
+  defp page_title(_) do
+    "Accessibility-First Development"
   end
 end
