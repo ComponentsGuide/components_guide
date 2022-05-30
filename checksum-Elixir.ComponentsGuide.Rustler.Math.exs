38c6aad2eb5ca2df8f3a9ddd6421dec658e86198
Patrick Smith <pgwsmith@gmail.com>
2022-05-30 17:27:01 +1000

    Bump rustler checksum
    Still not sure if this should be committed

--- checksum-Elixir.ComponentsGuide.Rustler.Math.exs
+++ checksum-Elixir.ComponentsGuide.Rustler.Math.exs
@@ -1,3 +1,3 @@
 %{
-  "libcomponentsguide_rustler_math-v0.1.0-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:ae5c68e74f726e7bea506743e1af3ab27f97786582b536aa7fd5ec4f821caf94",
+  "libcomponentsguide_rustler_math-v0.1.2-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:0e30ecf8c0614f9c0fffc553683ecef147caf0737e57304d2a37532f3cf9657d",
 }
