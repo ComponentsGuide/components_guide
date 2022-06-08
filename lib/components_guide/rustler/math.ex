ac9f30341f9529752fdb575ad49187b1285ca75f
Patrick Smith <pgwsmith@gmail.com>
2022-06-08 20:06:12 +1000

    Comment it all out!!

--- lib/components_guide/rustler/math.ex
+++ lib/components_guide/rustler/math.ex
@@ -1,21 +1,21 @@
 defmodule ComponentsGuide.Rustler.Math do
-  if false and Mix.env() == :dev do
-    use Rustler, otp_app: :components_guide, crate: :componentsguide_rustler_math
-    # use Rustler, otp_app: :components_guide, crate: :componentsguide_rustler_math, target_dir: System.tmp_dir!()
-    # use Rustler, otp_app: :components_guide, crate: :componentsguide_rustler_math, cargo: {:rustup, :stable}
-  end
+  # if false and Mix.env() == :dev do
+  #   use Rustler, otp_app: :components_guide, crate: :componentsguide_rustler_math
+  #   # use Rustler, otp_app: :components_guide, crate: :componentsguide_rustler_math, target_dir: System.tmp_dir!()
+  #   # use Rustler, otp_app: :components_guide, crate: :componentsguide_rustler_math, cargo: {:rustup, :stable}
+  # end
 
-  if false and Mix.env() == :prod do
-    app = Mix.Project.config()[:app]
-    version = Mix.Project.config()[:version]
+  # if false and Mix.env() == :prod do
+  #   app = Mix.Project.config()[:app]
+  #   version = Mix.Project.config()[:version]
 
-    use RustlerPrecompiled,
-      otp_app: app,
-      crate: "componentsguide_rustler_math",
-      base_url: "https://github.com/ComponentsGuide/components_guide/releases/download/v#{version}",
-      force_build: System.get_env("RUSTLER_PRECOMPILATION_EXAMPLE_BUILD") in ["1", "true"],
-      version: version
-  end
+  #   use RustlerPrecompiled,
+  #     otp_app: app,
+  #     crate: "componentsguide_rustler_math",
+  #     base_url: "https://github.com/ComponentsGuide/components_guide/releases/download/v#{version}",
+  #     force_build: System.get_env("RUSTLER_PRECOMPILATION_EXAMPLE_BUILD") in ["1", "true"],
+  #     version: version
+  # end
 
   def add(_, _), do: error()
   def reverse_string(_), do: error()
