71b1f4426b7ee29a30f58cba67bad4da0c9ac0c9
Patrick Smith <pgwsmith@gmail.com>
2022-06-08 19:23:21 +1000

    Update deps

--- mix.exs
+++ mix.exs
@@ -61,7 +61,7 @@ defmodule ComponentsGuide.MixProject do
       {:earmark, "~> 1.4.15"},
       {:paredown, "~> 0.1.0"},
       {:ex_image_info, "~> 0.2.4"},
-      {:rustler_precompiled, "~> 0.2.0"},
+      {:rustler_precompiled, "~> 0.5.1"},
       {:benchee, "~> 1.0", only: :dev},
       {:reverse_proxy_plug, "~> 1.3.1", only: :dev}
     ]
