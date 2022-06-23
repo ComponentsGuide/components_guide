1c522985e1bb5f1b058da188c729b8fb73627a4e
Patrick Smith <pgwsmith@gmail.com>
2022-06-23 21:27:20 +1000

    Allow importing headlessui from jspm.dev

--- lib/components_guide_web/controllers/react_editor_controller.ex
+++ lib/components_guide_web/controllers/react_editor_controller.ex
@@ -403,6 +403,165 @@ defmodule ComponentsGuideWeb.ReactEditorController do
     render_source(conn, source)
   end
 
+  def show(conn, %{"id" => "headlessui"}) do
+    source = ~s"""
+    import { Menu } from "https://jspm.dev/@headlessui/react"
+
+    export default function App() {
+      return <nav class="prose">
+        <Menu.Button>Hello</Menu.Button>
+        <ul>
+          <li><a href="/react-playground/react-elements" target="_blank">Understanding React Elements</a></li>
+          <li><a href="/react-playground/useid" target="_blank">useId()</a></li>
+          <li>
+            <a href="/react-playground/lists" target="_blank">Rendering Lists</a>
+            <ul>
+              <li><a href="/react-playground/lists2" target="_blank">Optimization A</a></li>
+              <li><a href="/react-playground/lists3" target="_blank">Optimization B</a></li>
+            </ul>
+          </li>
+          <li><a href="/react-playground/fetch" target="_blank">Fetch</a></li>
+          <li><a href="/react-playground/import-from-the-web" target="_blank">Importing from the Web</a></li>
+        </ul>
+      </nav>;
+    }
+    """
+
+    render_source(conn, source)
+  end
+
+  def show(conn, %{"id" => "headlessui-tabs"}) do
+    source = ~s"""
+    import { Tab } from "https://jspm.dev/@headlessui/react"
+
+    function classNames(...classes) {
+      return classes.filter(Boolean).join(' ')
+    }
+
+    export default function App() {
+      let [categories] = useState({
+        Recent: [
+          {
+            id: 1,
+            title: 'Does drinking coffee make you smarter?',
+            date: '5h ago',
+            commentCount: 5,
+            shareCount: 2,
+          },
+          {
+            id: 2,
+            title: "So you've bought coffee... now what?",
+            date: '2h ago',
+            commentCount: 3,
+            shareCount: 2,
+          },
+        ],
+        Popular: [
+          {
+            id: 1,
+            title: 'Is tech making coffee better or worse?',
+            date: 'Jan 7',
+            commentCount: 29,
+            shareCount: 16,
+          },
+          {
+            id: 2,
+            title: 'The most innovative things happening in coffee',
+            date: 'Mar 19',
+            commentCount: 24,
+            shareCount: 12,
+          },
+        ],
+        Trending: [
+          {
+            id: 1,
+            title: 'Ask Me Anything: 10 answers to your questions about coffee',
+            date: '2d ago',
+            commentCount: 9,
+            shareCount: 5,
+          },
+          {
+            id: 2,
+            title: "The worst advice we've ever heard about coffee",
+            date: '4d ago',
+            commentCount: 1,
+            shareCount: 2,
+          },
+        ],
+      })
+
+      return (
+        <div className="flex justify-center bg-gradient-to-r from-sky-400 to-blue-600">
+          <div className="w-full max-w-md px-2 py-16 sm:px-0">
+            <Tab.Group>
+              <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+                {Object.keys(categories).map((category) => (
+                  <Tab
+                    key={category}
+                    className={({ selected }) =>
+                      classNames(
+                        'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700',
+                        'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                        selected
+                          ? 'bg-white shadow'
+                          : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
+                      )
+                    }
+                  >
+                    {category}
+                  </Tab>
+                ))}
+              </Tab.List>
+              <Tab.Panels className="mt-2">
+                {Object.values(categories).map((posts, idx) => (
+                  <Tab.Panel
+                    key={idx}
+                    className={classNames(
+                      'rounded-xl bg-white p-3',
+                      'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
+                    )}
+                  >
+                    <ul>
+                      {posts.map((post) => (
+                        <li
+                          key={post.id}
+                          className="relative rounded-md p-3 hover:bg-gray-100"
+                        >
+                          <h3 className="text-sm font-medium leading-5">
+                            {post.title}
+                          </h3>
+
+                          <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
+                            <li>{post.date}</li>
+                            <li>&middot;</li>
+                            <li>{post.commentCount} comments</li>
+                            <li>&middot;</li>
+                            <li>{post.shareCount} shares</li>
+                          </ul>
+
+                          <a
+                            href="#"
+                            className={classNames(
+                              'absolute inset-0 rounded-md',
+                              'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2'
+                            )}
+                          />
+                        </li>
+                      ))}
+                    </ul>
+                  </Tab.Panel>
+                ))}
+              </Tab.Panels>
+            </Tab.Group>
+          </div>
+        </div>
+      )
+    }
+    """
+
+    render_source(conn, source)
+  end
+
   def show(conn, %{"id" => "userecursive"}) do
     source = ~s"""
     function useRecursive(initial) {
