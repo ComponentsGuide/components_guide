1bcb93398d10d7e81cca75cbb223e59cc571e6a1
Patrick Smith <pgwsmith@gmail.com>
2022-09-06 14:37:51 +1000

    Fix test for calendar component

--- test/collected_live_web/components/calendar_component_test.exs
+++ test/collected_live_web/components/calendar_component_test.exs
@@ -11,8 +11,20 @@ defmodule ComponentsGuideWeb.CalendarComponentTest do
       {:ok, el: el}
     end
 
-    @tag assigns: [date: ~D[2022-04-01]]
-    test "April 1 2022", %{el: el} do
+    @tag assigns: [year: 2022, month: 4]
+    test "April 2022", a do
+      %{el: el} = a
+      current_date = el |> find("td[aria-current=date]")
+      assert current_date |> Enum.count() == 0
+
+      assert 7 == count(el, "thead th")
+      assert 5 == count(el, "tbody tr")
+      assert 30 == count(el, "tbody td:not([role=presentation])")
+    end
+
+    @tag assigns: [year: 2022, month: 4, current_date: ~D[2022-04-01]]
+    test "April 2022 with 1st as current date", a do
+      %{el: el} = a
       current_date = el |> find("td[aria-current=date]")
       assert "1" == current_date |> text() |> String.trim()
       assert current_date |> Enum.count() == 1
@@ -22,14 +34,14 @@ defmodule ComponentsGuideWeb.CalendarComponentTest do
       assert 30 == count(el, "tbody td:not([role=presentation])")
     end
 
-    @tag assigns: [date: ~D[2022-04-15]]
-    test "April 15 2022", %{el: el} do
+    @tag assigns: [year: 2022, month: 4, current_date: ~D[2022-04-15]]
+    test "April 2022 with 15th as current date", %{el: el} do
       current_date = el |> find("td[aria-current=date]")
       assert "15" == current_date |> text() |> String.trim()
     end
 
-    @tag assigns: [date: ~D[2022-01-03]]
-    test "January 3 2022", %{el: el} do
+    @tag assigns: [year: 2022, month: 1, current_date: ~D[2022-01-03]]
+    test "January 2022 with 3rd as current date", %{el: el} do
       current_date = el |> find("td[aria-current=date]")
       assert "3" == current_date |> text() |> String.trim()
       assert 31 == count(el, "tbody td:not([role=presentation])")
