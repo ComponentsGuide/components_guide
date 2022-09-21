51153b21272b81119865b263eb5dd16728fcff87
Patrick Smith <pgwsmith@gmail.com>
2022-09-21 23:05:50 +1000

    Fetch.Request can be passed a URI struct

--- lib/components_guide/fetch/request.ex
+++ lib/components_guide/fetch/request.ex
@@ -1,15 +1,17 @@
 defmodule ComponentsGuide.Fetch.Request do
   defstruct method: "GET", url_string: "", uri: %URI{}, headers: [], body: nil
 
-  def new!(url_string, options \\ []) do
+  def new(uri_or_url_string, options \\ [])
+
+  def new(uri = %URI{}, options) do
     headers = Keyword.get(options, :headers, [])
     method = Keyword.get(options, :method, "GET")
     body = Keyword.get(options, :body)
-    uri = URI.new!(url_string)
+    url_string = URI.to_string(uri)
     %__MODULE__{method: method, url_string: url_string, uri: uri, headers: headers, body: body}
   end
 
-  def new(url_string, options \\ []) do
+  def new(url_string, options) when is_binary(url_string) do
     headers = Keyword.get(options, :headers, [])
     method = Keyword.get(options, :method, "GET")
     body = Keyword.get(options, :body)
@@ -20,5 +22,15 @@ defmodule ComponentsGuide.Fetch.Request do
     else
       {:error, _} = value -> value
     end
+  end
+
+  def new!(uri_or_url_string, options \\ [])
+
+  def new!(url_string, options) when is_binary(url_string) do
+    headers = Keyword.get(options, :headers, [])
+    method = Keyword.get(options, :method, "GET")
+    body = Keyword.get(options, :body)
+    uri = URI.new!(url_string)
+    %__MODULE__{method: method, url_string: url_string, uri: uri, headers: headers, body: body}
   end
 end
