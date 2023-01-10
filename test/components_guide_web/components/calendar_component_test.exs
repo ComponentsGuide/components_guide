b3f5833c3987b683b6111a02557c128c3d83547e
Patrick Smith <pgwsmith@gmail.com>
2023-01-10 12:50:53 +1100

    Fix calendar tests

--- test/components_guide_web/components/calendar_component_test.exs
+++ test/components_guide_web/components/calendar_component_test.exs
@@ -11,40 +11,28 @@ defmodule ComponentsGuideWeb.CalendarComponentTest do
       {:ok, el: el}
     end
 
-    @tag assigns: [year: 2022, month: 4]
-    test "April 2022", a do
-      %{el: el} = a
+    @tag assigns: [current_date: ~D[2022-04-01]]
+    test "1st April 2022", %{el: el} do
       current_date = el |> find("td[aria-current=date]")
-      assert current_date |> Enum.count() == 0
-
-      assert 7 == count(el, "thead th")
-      assert 5 == count(el, "tbody tr")
-      assert 30 == count(el, "tbody td:not([role=presentation])")
-    end
-
-    @tag assigns: [year: 2022, month: 4, current_date: ~D[2022-04-01]]
-    test "April 2022 with 1st as current date", a do
-      %{el: el} = a
-      current_date = el |> find("td[aria-current=date]")
-      assert "1" == current_date |> text() |> String.trim()
+      assert "01 Apr" == current_date |> text() |> String.trim()
       assert current_date |> Enum.count() == 1
 
       assert 7 == count(el, "thead th")
-      assert 5 == count(el, "tbody tr")
-      assert 30 == count(el, "tbody td:not([role=presentation])")
+      assert 9 == count(el, "tbody tr")
+      assert 9 * 7 == count(el, "tbody td:not([role=presentation])")
     end
 
-    @tag assigns: [year: 2022, month: 4, current_date: ~D[2022-04-15]]
-    test "April 2022 with 15th as current date", %{el: el} do
+    @tag assigns: [current_date: ~D[2022-04-15]]
+    test "15th April 2022", %{el: el} do
       current_date = el |> find("td[aria-current=date]")
-      assert "15" == current_date |> text() |> String.trim()
+      assert "15 Apr" == current_date |> text() |> String.trim()
     end
 
-    @tag assigns: [year: 2022, month: 1, current_date: ~D[2022-01-03]]
-    test "January 2022 with 3rd as current date", %{el: el} do
+    @tag assigns: [current_date: ~D[2022-01-03]]
+    test "3rd January 2022", %{el: el} do
       current_date = el |> find("td[aria-current=date]")
-      assert "3" == current_date |> text() |> String.trim()
-      assert 31 == count(el, "tbody td:not([role=presentation])")
+      assert "03 Jan" == current_date |> text() |> String.trim()
+      assert 9 * 7 == count(el, "tbody td:not([role=presentation])")
     end
   end
 end
