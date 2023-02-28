a473f897157a1a19fc2d6944fa7a1e0676db9c18
Patrick Smith <pgwsmith@gmail.com>
2023-02-28 16:34:26 +1100

    clean up

--- lib/components_guide_web/controllers/calendar_controller.ex
+++ lib/components_guide_web/controllers/calendar_controller.ex
@@ -2,21 +2,16 @@ defmodule ComponentsGuideWeb.CalendarController do
   use ComponentsGuideWeb, :controller
 
   def index(conn, _params) do
-    today = Date.utc_today()
-
-    assigns = [
+    render(conn, :index,
       page_title:
         "Calendar of when important tools are released, become LTS, and reach end-of-life",
-      today: today
-    ]
-
-    render(conn, "index.html", assigns)
+      today: Date.utc_today()
+    )
   end
 end
 
 defmodule ComponentsGuideWeb.CalendarHTML do
   use ComponentsGuideWeb, :html
-  use Phoenix.Component
 
   embed_templates("calendar_html/*")
 end
