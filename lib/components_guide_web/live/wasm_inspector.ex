afcacd9d07cd2fda9cd7632ef23350076e3327b5
Patrick Smith <pgwsmith@gmail.com>
2023-03-01 14:53:11 +1100

    Add suggestions to wasm url

--- lib/components_guide_web/live/wasm_inspector.ex
+++ lib/components_guide_web/live/wasm_inspector.ex
@@ -4,12 +4,14 @@ defmodule ComponentsGuideWeb.WasmInspectorLive do
 
   alias ComponentsGuide.Fetch
 
-  # https://raw.githubusercontent.com/rsms/markdown-wasm/v1.2.0/dist/markdown.wasm
-  # https://sqlite.org/wasm/doc/trunk/jswasm/sqlite3.wasm
-  # https://unpkg.com/@automerge/automerge-wasm@0.1.25/deno/automerge_wasm_bg.wasm
-  # https://raw.githubusercontent.com/GoogleChromeLabs/squoosh/v1.12.0/codecs/oxipng/pkg/squoosh_oxipng_bg.wasm
-  # https://unpkg.com/@bokuweb/zstd-wasm@0.0.17/dist/esm/wasm/zstd.wasm
-  # https://unpkg.com/esbuild-wasm@0.17.10/esbuild.wasm
+  @suggestions [
+    "https://raw.githubusercontent.com/rsms/markdown-wasm/v1.2.0/dist/markdown.wasm",
+    "https://sqlite.org/wasm/doc/trunk/jswasm/sqlite3.wasm",
+    "https://unpkg.com/@automerge/automerge-wasm@0.1.25/deno/automerge_wasm_bg.wasm",
+    "https://raw.githubusercontent.com/GoogleChromeLabs/squoosh/v1.12.0/codecs/oxipng/pkg/squoosh_oxipng_bg.wasm",
+    "https://unpkg.com/@bokuweb/zstd-wasm@0.0.17/dist/esm/wasm/zstd.wasm",
+    "https://unpkg.com/esbuild-wasm@0.17.10/esbuild.wasm"
+  ]
 
   defmodule State do
     alias ComponentsGuide.Rustler.Wasm
@@ -21,15 +23,18 @@ defmodule ComponentsGuideWeb.WasmInspectorLive do
 
     def set_wasm_bytes(%__MODULE__{} = state, wasm_bytes) do
       exports = Wasm.wasm_list_exports({:wasm, wasm_bytes})
+
       %__MODULE__{
         state
-        | wasm_bytes: wasm_bytes, exports: exports
+        | wasm_bytes: wasm_bytes,
+          exports: exports
       }
     end
   end
 
   @impl true
   def render(assigns) do
+    assigns = Map.put(assigns, :suggestions, @suggestions)
     ~H"""
     <.form
       for={@form}
@@ -38,23 +43,28 @@ defmodule ComponentsGuideWeb.WasmInspectorLive do
       class="max-w-2xl mx-auto space-y-2"
     >
       <div class="flex flex-col items-center gap-4">
+        <div class="w-full">
+          <.input
+            field={@form[:user_url]}
+            type="url"
+            label="URL"
+            placeholder="Enter URL to a .wasm module"
+            list="url_suggestions"
+          />
+          <datalist id="url_suggestions">
+            <option :for={suggestion <- @suggestions} value={suggestion}></option>
+          </datalist>
+        </div>
         <button type="submit" class="px-3 py-1 text-xl text-blue-900 bg-blue-200 rounded" autofocus>
           Load
         </button>
-        <.input
-          field={@form[:user_url]}
-          type="url"
-          label="URL"
-          placeholder="Enter URL to a .wasm module"
-        />
       </div>
     </.form>
 
     <output form="latency_comparison_form" class="flex flex-col gap-4 pt-4 max-w-none text-center">
       <%= if @state.wasm_bytes do %>
         <div><%= Format.humanize_bytes(byte_size(@state.wasm_bytes)) %></div>
-        <dt>
-        </dt>
+        <dt></dt>
         <.list>
           <:item :for={{type, name} <- @state.exports} title={inspect(type)}><%= name %></:item>
         </.list>
