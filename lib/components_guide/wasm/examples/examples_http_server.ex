27f024411c6f0ecf1cb877b565a539f4f11fb4d4
Patrick Smith <pgwsmith@gmail.com>
2023-06-17 19:57:53 +1000

    Clean up

--- lib/components_guide/wasm/examples/examples_http_server.ex
+++ lib/components_guide/wasm/examples/examples_http_server.ex
@@ -83,62 +83,6 @@ defmodule ComponentsGuide.Wasm.Examples.HTTPServer do
             # """
         end
       end
-
-      #       func to_string(),
-      #            I32.String,
-      #            str: I32,
-      #            byte_count: I32,
-      #            writer: I32,
-      #            name_len: I32,
-      #            value_len: I32,
-      #            domain_len: I32,
-      #            path_len: I32,
-      #            extra_len: I32 do
-      #         name_len = strlen(name)
-      #         value_len = strlen(value)
-      #         domain_len = strlen(domain)
-      #         path_len = strlen(path)
-      #
-      #         extra_len =
-      #           I32.add([
-      #             I32.when?(domain_len, do: I32.add(domain_len, byte_size("; Domain=")), else: 0),
-      #             I32.when?(path_len, do: I32.add(path_len, byte_size("; Path=")), else: 0),
-      #             I32.when?(secure, do: byte_size("; Secure"), else: 0),
-      #             I32.when?(http_only, do: byte_size("; HttpOnly"), else: 0)
-      #           ])
-      #
-      #         byte_count = I32.add([name_len, 1, value_len, extra_len])
-      #
-      #         # Add 1 for nul-terminator
-      #         str = alloc(I32.add(byte_count, 1))
-      #         writer = str
-      #
-      #         write!(name, name_len)
-      #         write!(ascii: ?=)
-      #         write!(value, value_len)
-      #
-      #         if domain_len do
-      #           write!(const("; Domain="))
-      #           write!(domain, domain_len)
-      #         end
-      #
-      #         if path_len do
-      #           write!(const("; Path="))
-      #           write!(path, path_len)
-      #         end
-      #
-      #         if secure do
-      #           write!(const("; Secure"))
-      #         end
-      #
-      #         if http_only do
-      #           write!(const("; HttpOnly"))
-      #         end
-      #
-      #         assert!(I32.eq(writer, I32.add(str, byte_count)))
-      #
-      #         str
-      #       end
     end
   end
 end
