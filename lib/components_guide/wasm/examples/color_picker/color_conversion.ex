91126ba08251c3aa41fbf559682543d0a073898c
Patrick Smith <pgwsmith@gmail.com>
2023-07-30 23:49:53 +1000

    Import pow into Orb from Elixir-land

--- lib/components_guide/wasm/examples/color_picker/color_conversion.ex
+++ lib/components_guide/wasm/examples/color_picker/color_conversion.ex
@@ -1,4 +1,5 @@
 defmodule ComponentsGuide.Wasm.Examples.ColorConversion do
+  require Orb.DSL
   use Orb
 
   @xn 0.96422
@@ -6,6 +7,14 @@ defmodule ComponentsGuide.Wasm.Examples.ColorConversion do
   @zn 0.82521
   @k :math.pow(29, 3) / :math.pow(3, 3)
   @e :math.pow(6, 3) / :math.pow(29, 3)
+
+  wasm_import(:math,
+    powf32: Orb.DSL.funcp(name: :powf32, params: {F32, F32}, result: F32)
+  #   # powf32: Orb.DSL.funcp(name: :powf32, params: F32, result: F32)
+  #   i32: Orb.DSL.funcp(name: :i32, params: I32, result: I32)
+  #   # f32: Orb.DSL.funcp(name: :f32, params: F32, result: F32)
+  )
+  wasm_import(:log, int32: Orb.DSL.funcp(name: :log32, params: I32, result: I32))
 
   wasm F32 do
     # Copied from: https://augustus-pash.gitbook.io/wasm/maths-algorithms/aprox-sin
@@ -45,6 +54,14 @@ defmodule ComponentsGuide.Wasm.Examples.ColorConversion do
       call(:lab_to_xyz_component, fx) * ^@xn
       call(:lab_to_xyz_component, fy) * ^@yn
       call(:lab_to_xyz_component, fz) * ^@zn
+    end
+
+    func srgb_to_linear_rgb_component(c: F32), F32 do
+      if c < 0.04045, result: F32 do
+        c / 12.92
+      else
+        call(:powf32, (c + 0.055) / 1.055, 2.4)
+      end
     end
   end
 end
--- test/components_guide/wasm/examples/color_picker_test.exs
+++ test/components_guide/wasm/examples/color_picker_test.exs
@@ -6,7 +6,21 @@ defmodule ComponentsGuide.Wasm.Examples.ColorPickerTest do
   alias OrbWasmtime.Instance
 
   test "ColorConversion" do
-    inst = Instance.run(ColorConversion)
+    inst = Instance.run(ColorConversion, [
+      {:math, :powf32,
+         fn x, y ->
+           Float.pow(x, y)
+         end},
+      # {:math, :f32,
+      #    fn x ->
+      #      1.0
+      #    end}
+      {:log, :int32,
+         fn value ->
+           IO.inspect(value, label: "wasm log int32")
+           0
+         end}
+    ])
     lab_to_xyz = Instance.capture(inst, :lab_to_xyz, 3)
     assert lab_to_xyz.(100.0, 0.0, 0.0) === {0.9642199873924255, 1.0, 0.8252099752426147}
   end
