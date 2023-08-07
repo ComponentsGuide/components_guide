dd1d2af297b12042a0295a757e8f5d71b6559bee
Patrick Smith <pgwsmith@gmail.com>
2023-08-07 21:16:05 +1000

    Improve lab swatch

--- lib/components_guide/wasm/examples/color_picker/lab_swatch.ex
+++ lib/components_guide/wasm/examples/color_picker/lab_swatch.ex
@@ -24,16 +24,28 @@ defmodule ComponentsGuide.Wasm.Examples.LabSwatch do
 
   # I32.export_global(:mutable, l: 50)
 
-  I32.enum([:swatch_l, :swatch_a, :swatch_b])
+  F32.export_global(:mutable,
+    l: 50.0,
+    a: 100,
+    b: -128
+  )
 
-  wasm do
+  I32.enum([:component_l, :component_a, :component_b])
+
+  wasm F32 do
     ColorConversion.funcp()
 
     # import_funcp :math, powf32(x: F32, y: F32), F32
 
+    func to_html(), I32.String do
+      build! do
+        append!(:swatch_svg, @component_l)
+      end
+    end
+
     func to_svg(), I32.String do
       build! do
-        append!(:swatch_svg, @swatch_l)
+        append!(:swatch_svg, @component_l)
       end
     end
 
@@ -44,20 +56,32 @@ defmodule ComponentsGuide.Wasm.Examples.LabSwatch do
         )
 
         append!(~S(<defs>\n))
-        append!(:do_linear_gradient)
+        append!(:do_linear_gradient, @component_l)
         append!(~S(</defs>\n))
         append!(~S{<rect width="1" height="1" fill="url('#lab-l-gradient')" />\n})
 
         # append!(~S{<circle data-drag-knob cx="<%= l / 100.0 %>" cy="<%= l / 100.0 %>" r="0.05" fill="white" stroke="black" stroke-width="0.01" />})
-        append!(
-          ~S{<circle data-drag-knob="" cx="0.5" cy="0.5" r="0.05" fill="white" stroke="black" stroke-width="0.01" />\n}
-        )
+        append!(:do_drag_knob, 0.5)
 
         append!(~S{</svg>\n})
       end
     end
 
-    funcp do_linear_gradient(), I32.String, i: I32 do
+    funcp do_drag_knob(offset: F32), I32.String do
+      build! do
+        append!(~S{<circle data-drag-knob="" cx="})
+        append!(decimal_f32: offset)
+        append!(~S{" cy="})
+        append!(decimal_f32: offset)
+        append!(~S{" r="0.05" fill="white" stroke="black" stroke-width="0.01" />\n})
+      end
+    end
+
+    funcp interpolate(t: F32, lowest: F32, highest: F32), F32 do
+      (highest - lowest) * t + lowest
+    end
+
+    funcp do_linear_gradient(component_index: I32), I32.String, i: F32 do
       build! do
         append!(
           string: ~S{<linearGradient id="},
@@ -66,15 +90,22 @@ defmodule ComponentsGuide.Wasm.Examples.LabSwatch do
         )
 
         loop Stops do
-          i = i + 1
-          Stops.continue(if: i < 20)
+          append!(:do_linear_gradient_stop, [
+            i / 4.0,
+            call(:interpolate, i / 4.0, 0.0, 100.0),
+            0.0,
+            0.0
+          ])
+
+          i = i + 1.0
+          Stops.continue(if: i <= 4.0)
         end
 
-        append!(:do_linear_gradient_stop, [0.0, 0.0, 0.0, 0.0])
-        append!(:do_linear_gradient_stop, [0.25, 25.0, 0.0, 0.0])
-        append!(:do_linear_gradient_stop, [0.5, 50.0, 0.0, 0.0])
-        append!(:do_linear_gradient_stop, [0.75, 75.0, 0.0, 0.0])
-        append!(:do_linear_gradient_stop, [1.0, 100.0, 0.0, 0.0])
+        # append!(:do_linear_gradient_stop, [0.0, 0.0, 0.0, 0.0])
+        # append!(:do_linear_gradient_stop, [0.25, 25.0, 0.0, 0.0])
+        # append!(:do_linear_gradient_stop, [0.5, 50.0, 0.0, 0.0])
+        # append!(:do_linear_gradient_stop, [0.75, 75.0, 0.0, 0.0])
+        # append!(:do_linear_gradient_stop, [1.0, 100.0, 0.0, 0.0])
         append!(~S{</linearGradient>\n})
       end
     end
