1c11af0f6ef1eada7f9d42d055241d3fc8cff290
Patrick Smith <pgwsmith@gmail.com>
2023-09-15 16:50:14 +1000

    Use MemoryIO in <wasm-chunked-html>

--- assets/js/customElements/wasm-chunked-html.js
+++ assets/js/customElements/wasm-chunked-html.js
@@ -1,3 +1,5 @@
+import { MemoryIO } from "../wasm/memoryIO";
+
 class WasmChunkedHTML extends HTMLElement {
   connectedCallback() {
     console.log("WASM CONNECTED", this)
@@ -9,10 +11,10 @@ class WasmChunkedHTML extends HTMLElement {
           buffer: memory
         }
       })
-      .then(a => {
-        console.log("Loaded wasm directly", Date.now() - window.startTime);
-        return a;
-      });
+        .then(a => {
+          console.log("Loaded wasm directly", Date.now() - window.startTime);
+          return a;
+        });
       initWasmHTML(this, wasmInstancePromise, memory);
     }
 
@@ -42,56 +44,40 @@ class WasmChunkedHTML extends HTMLElement {
   }
 }
 
-function initWasmHTML(el, wasmInstancePromise) {
-  wasmInstancePromise.then(({ instance }) => {
-    const memory = instance.exports.memory;
-    const rewind = instance.exports.rewind;
-    const next_body_chunk = instance.exports.next_body_chunk;
+async function initWasmHTML(el, wasmInstancePromise) {
+  const { instance } = await wasmInstancePromise;
+  const rewind = instance.exports.rewind;
+  const next_body_chunk = instance.exports.next_body_chunk;
 
-    const memoryBytes = new Uint8Array(memory.buffer);
-    //const memoryToWrite = memoryBytes.subarray(1024);
-    //const memoryToRead = memoryBytes.subarray(2048);
-    const utf8encoder = new TextEncoder();
-    const utf8decoder = new TextDecoder();
+  const memoryIO = new MemoryIO(instance.exports);
 
-    function update() {
-      rewind?.call();
+  function update() {
+    rewind?.call();
 
-      const chunks = [];
-      //const chunks = new Uint8Array(1000);
-      while (true) {
-        const ptr = next_body_chunk();
-        if (ptr === 0) {
-          break;
-        }
-
-        // Search for null-terminating byte.
-        const endPtr = memoryBytes.indexOf(0, ptr);
-        // Get subsection of memory between start and end, and decode it as UTF-8.
-        //return utf8decoder.decode(memoryBytes.subarray(ptr, endPtr));
-        //chunks.concat(memoryToRead.subarray(0, count));
-        chunks.push(memoryBytes.subarray(ptr, endPtr));
-        //chunks.set(memoryBytes.subarray(ptr, endPtr), chunks.length);
+    const stringChunks = [];
+    while (true) {
+      const ptr = next_body_chunk();
+      if (ptr === 0) {
+        break;
       }
 
-      // There surely must be a better way to do this.
-      // See: https://stackoverflow.com/questions/49129643/how-do-i-merge-an-array-of-uint8arrays
-      const bytes = new Uint8Array(chunks.map(chunk => [...chunk]).flat());
-      const html = utf8decoder.decode(bytes);
-      console.log("wasm-html render", html)
-      el.innerHTML = html;
+      stringChunks.push(memoryIO.readString(ptr));
     }
 
-    el.addEventListener("click", (event) => {
-      const action = event.target.dataset.action;
-      if (typeof action === "string") {
-        instance.exports[action]?.apply();
-      }
-      update();
-    });
+    const html = stringChunks.join("");
+    console.log("wasm-html render", html)
+    el.innerHTML = html;
+  }
 
-    queueMicrotask(update);
+  el.addEventListener("click", (event) => {
+    const action = event.target.dataset.action;
+    if (typeof action === "string") {
+      instance.exports[action]?.apply();
+    }
+    update();
   });
+
+  queueMicrotask(update);
 }
 
 customElements.define("wasm-chunked-html", WasmChunkedHTML);
