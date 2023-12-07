75e1af3ef1f8481c6f132b053dd5633fe19e6d45
Patrick Smith <pgwsmith@gmail.com>
2023-12-07 12:57:15 +1100

    Sketch for epub builder

---
+++ lib/components_guide/wasm/examples/epub/epub_builder.ex
@@ -0,0 +1,5 @@
+defmodule ComponentsGuide.Wasm.Examples.Epub.Builder do
+  # https://opensource.com/article/22/8/epub-file
+  # https://github.com/futurepress/epub.js
+  # https://idpf.github.io/epub3-samples/30/samples.html
+end
