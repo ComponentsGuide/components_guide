b85226aceb92eb5611679467f0fe37fc270123c0
Patrick Smith <pgwsmith@gmail.com>
2024-01-15 22:53:49 +1100

    Compare json to json embedded in wasm byte sizes

--- lib/components_guide/wasm/examples/examples_magic_numbers.ex
+++ lib/components_guide/wasm/examples/examples_magic_numbers.ex
@@ -13,8 +13,26 @@ defmodule ComponentsGuide.Wasm.Examples.MagicNumbers do
       fast_3g_download: 180_000,
       fast_3g_upload: 84_375
     )
+  end
 
-    wasm do
+  defmodule MobileThrottlingJSON do
+    use Orb
+
+    Memory.pages(1)
+
+    def json do
+      Jason.encode!(%{
+        slow_3g_latency_ms: 2000,
+        slow_3g_download: 50_000,
+        slow_3g_upload: 50_000,
+        fast_3g_latency_ms: 563,
+        fast_3g_download: 180_000,
+        fast_3g_upload: 84_375
+      })
+    end
+
+    defw application_json, I32.String do
+      json()
     end
   end
 end
--- test/components_guide/wasm/examples/magic_numbers_test.exs
+++ test/components_guide/wasm/examples/magic_numbers_test.exs
@@ -6,21 +6,13 @@ defmodule ComponentsGuide.Wasm.Examples.MagicNumbers.Test do
 
   describe "MobileThrottling" do
     alias MagicNumbers.MobileThrottling
+    alias MagicNumbers.MobileThrottlingJSON
 
-    test "byte size vs json" do
-      assert byte_size(Wasm.to_wasm(MobileThrottling)) == 168
+    test "json encoded size", do: assert(149 = byte_size(MobileThrottlingJSON.json()))
+    test "wasm with globals size", do: assert(168 = byte_size(Wasm.to_wasm(MobileThrottling)))
 
-      assert byte_size(
-               Jason.encode!(%{
-                 slow_3g_latency_ms: 2000,
-                 slow_3g_download: 50_000,
-                 slow_3g_upload: 50_000,
-                 fast_3g_latency_ms: 563,
-                 fast_3g_download: 180_000,
-                 fast_3g_upload: 84_375
-               })
-             ) == 149
-    end
+    test "wasm with embedded json size",
+      do: assert(224 = byte_size(Wasm.to_wasm(MobileThrottlingJSON)))
 
     @tag :skip
     test "opt" do
@@ -28,7 +20,7 @@ defmodule ComponentsGuide.Wasm.Examples.MagicNumbers.Test do
       path_wat = Path.join(__DIR__, "mobile_throttling.wat")
       path_opt_wasm = Path.join(__DIR__, "mobile_throttling_OPT.wasm")
       path_opt_wat = Path.join(__DIR__, "mobile_throttling_OPT.wat")
-      wasm = Wasm.to_wasm(MobileThrottling)
+      wasm = Wasm.to_wasm(MobileThrottlingJSON)
       File.write!(path_wasm, wasm)
       System.cmd("wasm-opt", [path_wasm, "-o", path_opt_wasm, "-O"])
 
