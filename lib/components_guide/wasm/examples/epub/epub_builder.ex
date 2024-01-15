0ed7a53f9c874f3cde2383a18983c60294cd5090
Patrick Smith <pgwsmith@gmail.com>
2024-01-15 22:45:24 +1100

    Another epub link

--- lib/components_guide/wasm/examples/epub/epub_builder.ex
+++ lib/components_guide/wasm/examples/epub/epub_builder.ex
@@ -2,4 +2,5 @@ defmodule ComponentsGuide.Wasm.Examples.Epub.Builder do
   # https://opensource.com/article/22/8/epub-file
   # https://github.com/futurepress/epub.js
   # https://idpf.github.io/epub3-samples/30/samples.html
+  # https://github.com/standardebooks/bertrand-russell_roads-to-freedom/blob/master/src/epub/toc.xhtml
 end
