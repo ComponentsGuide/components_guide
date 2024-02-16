191c83f9fd904190f2226b8031435b42ab93ab2e
Patrick Smith <pgwsmith@gmail.com>
2024-02-16 15:31:54 +1100

    Calendar grid sketch

---
+++ lib/components_guide/wasm/examples/calendar/calendar_grid.ex
@@ -0,0 +1,14 @@
+defmodule ComponentsGuide.Wasm.Examples.Calendar.Grid do
+  # Displays an HTML calendar grid for a given year and month.
+  # Uses Elixir’s Date, Date.Range, Calendar.ISO to grab all the data for each year.
+
+  for year <- 1970..2100 do
+
+  end
+
+
+  # testw "leap_year?" do
+  #   assert call(:leap_year?, 2000) = Calendar.ISO.leap_year?(2000)
+  #   # Property-based testing
+  # end
+end
